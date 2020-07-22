f894b9d25f6c87b4f2d3327c2ce2f13243c13db3
Prasanta <pkd.dapacific@gmail.com>
2020-07-22 23:06:45 +0530

    basic chart made functional

--- src/lib/chart.js
+++ src/lib/chart.js
@@ -11,21 +11,29 @@ class CountryRankingStrips extends ChartComponent {
     },
     distributionProps: {
       bandwidth: 0.5,
-      xTicks: 10,
+      threshold: 30,
+      xTicks: 5,
+      curveType: 'curveBasis',
     },
-    height: 200,
+    height: 250,
     margin: {
       top: 20,
       right: 20,
       bottom: 20,
       left: 20,
     },
+    markDataPoint: [
+      {
+        key: 'IN',
+        text: 'India',
+      },
+    ],
   };
 
   defaultData = defaultData;
 
   draw() {
-    const data = this.data();
+    const allData = this.data();
     const props = this.props();
     const node = this.selection().node();
 
@@ -35,27 +43,29 @@ class CountryRankingStrips extends ChartComponent {
       .duration(750);
 
     // DEFINE SCALES
+    const data = allData.filter(d => d[props.dataParams.value]);
     const dataValues = data.map(d => d[props.dataParams.value]);
+    // console.log(d3.extent(dataValues));
     const xScale = d3.scaleLinear()
-      .domain(d3.extent(dataValues))
+      .domain(d3.extent(dataValues)).nice()
       .range([props.margin.left, width - props.margin.right]);
 
-    const thresholds = xScale.ticks(props.distributionProps.xTicks);
+    const thresholds = xScale.ticks(props.distributionProps.threshold);
     const bins = d3.histogram()
       .domain(xScale.domain())
       .thresholds(thresholds)(dataValues);
 
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(bins, d => d.length) / data.length])
-      .range([props.height - props.margin.bottom - 24, props.margin.top]);
+      .range([props.height - props.margin.bottom, props.margin.top]);
 
     const distributionLine = d3.line()
-      .curve(d3.curveBasis)
+      .curve(d3[props.distributionProps.curveType])
       .x(d => xScale(d[0]))
       .y(d => yScale(d[1]));
 
     const distributionArea = d3.area()
-      .curve(d3.curveBasis)
+      .curve(d3[props.distributionProps.curveType])
       .x(d => xScale(d[0]))
       .y1(d => yScale(d[1]))
       .y0(d => yScale(0));
@@ -69,6 +79,8 @@ class CountryRankingStrips extends ChartComponent {
     };
 
     const density = kde(epanechnikov(props.distributionProps.bandwidth), thresholds, dataValues);
+
+    // console.log(yScale.domain());
 
     console.log('density', density);
     console.log('bins', bins);
@@ -88,29 +100,35 @@ class CountryRankingStrips extends ChartComponent {
       .call(
         d3.axisBottom(xScale)
           .tickValues(thresholds)
+          // .tickValues(xScale.domain())
+          // .ticks(props.distributionProps.xTicks)
           // .tickFormat(dateFormat)
       );
 
-    chartSVG.appendSelect('g.axis-y');
+    // chartSVG.appendSelect('g.axis-y')
+    //   .attr('transform', `translate(${props.margin.left},0)`)
+    //   .call(d3.axisLeft(yScale).ticks(null, '%'))
+    //   .call(g => g.select('.domain').remove());
 
     // add distribution path
-
     const plot = chartSVG.appendSelect('g.plot')
       .attr('class', 'plot');
 
-    const rugs = plot.append('g').selectAll('rect')
-      .data(data);
-    rugs.enter().append('rect')
-      .attr('x', d => xScale(d.value))
-      .attr('y', props.height - props.margin.bottom - 24)
-      .attr('height', 24)
-      .attr('width', 2)
-      .style('mix-blend-mode', 'multiply')
-      .attr('stroke', 1)
-      .attr('fill', 'orange');
+    // RUGPLOT
+    // const rugs = plot.append('g').selectAll('rect')
+    //   .data(data.filter(d => d.value >= 19 && d.value <= 28));
+    // rugs.enter().append('rect')
+    //   .attr('class', d => `${d.key}`)
+    //   .attr('data-value', d => `${d.value}`)
+    //   .attr('x', d => xScale(d.value))
+    //   .attr('y', props.margin.top)
+    //   .attr('height', props.height - props.margin.top - props.margin.bottom)
+    //   .attr('width', width / data.length)
+    //   .style('mix-blend-mode', 'multiply')
+    //   .attr('stroke', 1)
+    //   .attr('fill', 'orange');
 
-    // const distPath = plot.selectAll('path');
-
+    // HISTOGRAM CODE
     // plot.append('g')
     //   .attr('fill', '#bbb')
     //   .selectAll('rect')
@@ -118,28 +136,93 @@ class CountryRankingStrips extends ChartComponent {
     //   .join('rect')
     //   .attr('x', d => xScale(d.x0) + 1)
     //   .attr('y', d => {
-    //     console.log(d.length, data.length);
+    //     // console.log(d.length, data.length);
     //     return yScale(d.length / data.length);
     //   })
     //   .attr('width', d => xScale(d.x1) - xScale(d.x0) - 1)
     //   .attr('height', d => yScale(0) - yScale(d.length / data.length));
 
-    plot.appendSelect('path.area')
+    plot.appendSelect('path.distribution-area')
+      .attr('class', 'area distribution-area')
       .datum(density)
       .transition(transition)
-      .attr('fill', 'orange')
-      .attr('stroke', 'none')
       .attr('stroke-linejoin', 'round')
       .attr('d', distributionArea);
 
-    plot.appendSelect('path.line')
+    console.log('area', d3.polygonArea((density)));
+
+    plot.appendSelect('path.distribution-line')
+      .attr('class', 'line distribution-line')
       .datum(density)
       .transition(transition)
-      .attr('fill', 'none')
-      .attr('stroke', 'red')
-      .attr('stroke-width', 1.5)
       .attr('stroke-linejoin', 'round')
       .attr('d', distributionLine);
+
+    // HIGHLIGHT DATA PONT
+
+    // add distrubution clipping mask
+    const svgDefs = chartSVG.appendSelect('defs');
+
+    svgDefs.appendSelect('clipPath')
+      .attr('id', 'clip-path')
+      .appendSelect('path')
+      .attr('d', distributionArea(density));
+
+    // add highlight shape
+    const highlightGroup = chartSVG.appendSelect('g.highlights')
+      .attr('class', 'highlights');
+    const highlights = highlightGroup.selectAll('rect')
+      .data(props.markDataPoint);
+
+    highlights.enter().append('rect')
+      .attr('class', d => `${d.key}`)
+      .attr('data-value', d => {
+        return (d.value) ? `${d.value}` : `${allData.find(e => e.key === d.key)[props.dataParams.value]}`;
+      })
+      .attr('x', d => d.value ? xScale(d.value) : xScale(allData.find(e => e.key === d.key)[props.dataParams.value]))
+      .attr('y', props.height - props.margin.bottom)
+      .attr('height', 0)
+      .attr('width', width / data.length)
+      .style('clip-path', 'url(#clip-path)')
+      .merge(highlights)
+      .transition(transition.delay(0).duration(1000))
+      .attr('class', d => `${d.key}`)
+      .attr('data-value', d => {
+        return (d.value) ? `${d.value}` : `${allData.find(e => e.key === d.key)[props.dataParams.value]}`;
+      })
+      .attr('x', d => d.value ? xScale(d.value) : xScale(allData.find(e => e.key === d.key)[props.dataParams.value]))
+      .attr('y', props.margin.top)
+      .attr('height', props.height - props.margin.top - props.margin.bottom)
+      .attr('width', width / data.length)
+      .style('clip-path', 'url(#clip-path)');
+
+    highlights.exit()
+      .attr('height', 0)
+      .transition(transition)
+      .remove();
+
+    // FOR HIGHLIGHTING THE BIN WITH THE DATA POINT
+    // const dataHighlight = bins.find(element => {
+    //   return element.includes(6.2960862166761205);
+    // });
+
+    // console.log((dataHighlight));
+
+    // const pathHighlight = (d) => {
+    //   return [
+    //     [(d.x0), (0)],
+    //     [(d.x0), (0.3)],
+    //     [(d.x1), (0.3)],
+    //     [(d.x1), (0)],
+    //   ];
+    // };
+    // console.log(pathHighlight(dataHighlight));
+    // plot.appendSelect('path.area-highlight')
+    //   .attr('class', 'area-highlight')
+    //   .datum(pathHighlight(dataHighlight))
+    //   .attr('fill', 'red')
+    //   .style('clip-path', 'url(#clip-path)')
+    //   .attr('d', distributionArea);
 
     return this;
   }
